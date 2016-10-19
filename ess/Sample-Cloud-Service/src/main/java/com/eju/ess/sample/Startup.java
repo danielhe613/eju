a679e01004315203ee48a557e222aded5a2779f0
zhaoq <601476362@qq.com>
2016-10-19 18:38:20 +0800

    修改服务下线从注册中心快速移除服务配置

--- ess/Sample-Cloud-Service/src/main/java/com/eju/ess/sample/Startup.java
+++ ess/Sample-Cloud-Service/src/main/java/com/eju/ess/sample/Startup.java
@@ -3,6 +3,7 @@ package com.eju.ess.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Startup {
--- ess/Sample-Cloud-UI/src/main/java/com/eju/ess/sample/Startup.java
+++ ess/Sample-Cloud-UI/src/main/java/com/eju/ess/sample/Startup.java
@@ -5,19 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
-@EnableDiscoveryClient // 启用发现、注册
-@EnableCircuitBreaker  // 启用熔断机制
+//@EnableDiscoveryClient // 启用发现、注册
+//@EnableCircuitBreaker  // 启用熔断机制
+@EnableZuulProxy
 public class Startup {
 
-	@Bean
+	/*@Bean
 	@LoadBalanced // template做LB
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
-	}
+	}*/
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
 	}
